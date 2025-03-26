82910ddaf46dc18bb2ddb56e83078cac09c3bfae
patiencemanzen <hseal419@gmail.com>
2025-03-26 16:46:07 +0200

    test

--- src/components/sections/PreservedMemories.tsx
+++ src/components/sections/PreservedMemories.tsx
@@ -7,16 +7,16 @@ function PreservedMemories() {
             <div className="container mx-auto px-4">
                 <div className="mb-12 md:mb-20 max-w-5xl">
                     <h1 className="relative text-3xl md:text-5xl text-white font-semibold font-boldonse">
-                        <span className="mr-3 leading-[4rem]">Resilience, Legacy,</span>              
+                        <span className="mr-3 leading-[4rem]">Resilience, Legacy,</span>
                         <span className="relative inline-block">
-                            <span className="relative z-10 leading-[5rem]">and Power of Memory</span>                
+                            <span className="relative z-10 leading-[5rem]">and Power of Memory</span>
                             <span className="absolute -bottom-5 left-0 h-2.5 w-full bg-green-100 rounded-full"></span>
                         </span>
                     </h1>
                 </div>
 
                 <h3 className="mb-8 text-4xl text-white font-semibold tracking-tight font-heading">Discuss the social, cultural, and emotional benefits.</h3>
-                
+
                 <div className="flex flex-wrap -m-3">
                     <div className="w-full md:w-1/2 xl:w-1/3 p-3">
                         <div className="flex flex-col h-full justify-center p-10 bg-white rounded-3xl">
@@ -25,7 +25,7 @@ function PreservedMemories() {
                                     <div className="flex items-center justify-center w-16 h-16 bg-neutral-900 rounded-full"><img src="template-assets/images/features/sticker.svg" alt="" /></div>
                                 </div>
                                 <div className="flex-1 p-3">
-                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800 text-slate-700">Customized Campaigns</h4>
+                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800 text-slate-700">Past Memories</h4>
                                 </div>
                             </div>
                             <p className="text-neutral-600 font-medium tracking-tight max-w-sm">Tailor your marketing approach with our customizable campaigns. We work closely with you to understand your unique goals and audience, designing personalized strategies that ensure your message reaches the right people at the right time.</p>
@@ -38,7 +38,7 @@ function PreservedMemories() {
                                     <div className="flex items-center justify-center w-16 h-16 bg-neutral-900 rounded-full"><img src="template-assets/images/features/sticker.svg" alt="" /></div>
                                 </div>
                                 <div className="flex-1 p-3">
-                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800">Data-Driven Insights</h4>
+                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800">Imagery Arts</h4>
                                 </div>
                             </div>
                             <p className="text-neutral-600 font-medium tracking-tight max-w-sm">Unlock the power of data with our advanced analytics tools. Gain actionable insights into customer behavior, market trends, and campaign performance</p>
@@ -49,7 +49,7 @@ function PreservedMemories() {
                                     <div className="flex items-center justify-center w-16 h-16 bg-neutral-900 rounded-full"><img src="template-assets/images/features/sticker.svg" alt="" /></div>
                                 </div>
                                 <div className="flex-1 p-3">
-                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800">Customer Support</h4>
+                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800"> Story that Inspires </h4>
                                 </div>
                             </div>
                             <p className="text-neutral-600 font-medium tracking-tight max-w-sm">We're here for you around the clock. Our dedicated support team is ready to assist with any questions or concerns you may have.</p>
@@ -62,7 +62,7 @@ function PreservedMemories() {
                                     <div className="flex items-center justify-center w-16 h-16 bg-neutral-900 rounded-full"><img src="template-assets/images/features/sticker.svg" alt="" /></div>
                                 </div>
                                 <div className="flex-1 p-3">
-                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800">A/B Testing and Optimization</h4>
+                                    <h4 className="text-4xl font-medium tracking-tight font-heading max-w-xs text-slate-800">Restoring the Past</h4>
                                 </div>
                             </div>
                             <p className="text-neutral-600 font-medium tracking-tight max-w-sm">Maximize the effectiveness of your digital campaigns through A/B testing and continuous optimization. We fine-tune your content, ads, and user experience to increase engagement, conversions, and overall success.</p>
