c24b282ad20c6c878bd67d8f0ff12762198dbf8d
patiencemanzen <hseal419@gmail.com>
2025-04-07 15:48:23 +0200

    Update About Us section for clarity, enhance MemoriesList component responsiveness, modify Hero section social links, and improve Download section messaging

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -26,24 +26,37 @@ function AboutUs() {
                                 </div>
                             </div>
 
-                            <div className="flex items-center justify-start lg:order-1 sm:py-16 lg:py-16 xl:py-32">
+                            <div className="flex items-center justify-start lg:order-1 sm:py-16 lg:py-10 xl:py-28">
                                 <div>
                                     <p className="text-sm font-semibold tracking-widest text-gray-500 uppercase">About us</p>
-                                    <h2 className="mt-8 text-3xl font-bold font-boldonse leading-tight text-white sm:text-4xl lg:text-5xl" style={{ lineHeight: '4rem'}}>
+                                    <h2 className="mt-8 text-3xl font-bold font-boldonse leading-tight text-white sm:text-4xl lg:text-5xl" style={{ lineHeight: '4rem' }}>
                                         All About Preserving Memories
                                     </h2>
                                     <p className="text-xl leading-relaxed text-gray-200 mt-9">
-                                        During the 29th commemoration of the 1994 Genocide Against the Tutsi, King NGABO engaged in a poignant conversation with a genocide survivor. The survivor expressed a deeply personal loss, revealing, "Young brother, do you know that we don’t have pictures of our parents?"
+                                        Once, during the 29th Commemoration of the 1994 Genocide Against the Tutsi, King NGABO had a conversation with a Genocide Survivor.
                                     </p>
-                                    <a href="#" title="" className="inline-flex items-center justify-center px-10 py-4 mt-12 text-base font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700" role="button"> Explore more </a>
                                 </div>
                             </div>
                         </div>
 
                         <p className='text-xl leading-relaxed text-gray-200'>
-                            When King NGABO inquired about the circumstances, the survivor explained that the last photo they possessed was misplaced during a move in 2005. Curious about family legacy, King NGABO asked how the survivor recounted stories of their grandparents to their children. With a gentle laugh, the survivor responded, "I tell them that every old man is their grandfather and every old woman is their grandmother."
-                            This exchange profoundly affected King NGABO and ignited a spark of inspiration. While collaborating with students from Nyundo School of Arts at his studio—a place where students frequently spent their holidays—King NGABO encountered a post about the only existing, albeit unclear, picture of someone's grandfather. Motivated by this discovery, the studio team restored the image and returned it to the appreciative family.
-                            This incident propelled King NGABO to take a significant step forward. He resolved to establish a platform focused on the restoration and preservation of images damaged during the 1994 Genocide Against the Tutsi. This initiative led to the creation of "ART FOR MEMORIES," a project committed to restoring these important photographs and compiling, archiving, and artistically representing the stories of genocide survivors.
+                            The survivor said:
+                            “Young brother, do you know that we don’t have pictures of our parents?”
+                            King NGABO asked, “What happened?”
+                            The survivor replied, “When we moved to a new house in 2005, the last picture we had was lost.”
+                            King NGABO then asked, “How do you tell your children about their grandparents?”
+
+                            With a soft laugh, the survivor said, “I tell them that every old man is their grandfather and every old woman is their grandmother.”
+
+                            That moment deeply touched King NGABO and sparked an idea in his mind.
+
+                            During a studio session, King NGABO was working alongside students from Nyundo School of Arts, who often spent their holidays at his studio. He came across a post where someone shared the only existing picture of their grandfather, though it was unclear. Seeing this, the studio team worked on restoring the image and returned it to the family.
+
+                            This experience inspired King NGABO to take action.
+
+                            He decided to create a platform dedicated to preserving the damaged images of the victims of the 1994 Genocide Against the Tutsi.
+
+                            From this vision, “ART FOR MEMORIES” was born, a project dedicated to preserving the damaged images of the victims, as well as collecting, archiving, and illustrating the stories of Genocide Survivors through art.
                         </p>
                     </div>
                 </section>
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -102,16 +102,16 @@ const Hero = () => {
             {/* Social Icons */}
             <div className="hidden sm:hidden md:block lg:block relative">
                 <div className="absolute right-0 top-1/2 transform -translate-y-1/2 flex flex-col gap-4 text-white">
-                    <a href="#" className="hover:text-gray-400">
+                    <a href="https://www.instagram.com/artformemories_rw?igsh=MXVxeTl0OHFndm8zdg==" className="hover:text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"><path opacity=".4" d="M14 9.3v2.95h2.63c.19 0 .33.17.29.36l-.38 1.9c-.03.14-.15.24-.29.24H14V22h-3v-7.25H9.3c-.17 0-.3-.13-.3-.3v-1.9c0-.17.13-.3.3-.3H11V9c0-1.66 1.34-3 3-3h2.7c.17 0 .3.13.3.3v2.4c0 .17-.13.3-.3.3h-2.4c-.17 0-.3.13-.3.3Z" stroke="#ffffff" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round"></path><path d="M15 22H9c-5 0-7-2-7-7V9c0-5 2-7 7-7h6c5 0 7 2 7 7v6c0 5-2 7-7 7Z" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                     </a>
-                    <a href="#" className="hover:text-gray-400">
+                    <a href="https://x.com/artformemories?s=21&t=4pzHCCUqdtJlXghNpFYuIQ" className="hover:text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"><path d="M5.46 2H4.05c-.93 0-1.4 1.13-.74 1.79l5.73 5.73a4.189 4.189 0 0 0 5.93 0l5.73-5.73c.66-.66.19-1.79-.74-1.79h-1.41c-.83 0-1.63.33-2.22.92L12.75 6.5c-.41.41-1.07.41-1.48 0L7.69 2.92C7.09 2.33 6.29 2 5.46 2Z" stroke="#ffffff" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path><path opacity=".4" d="M5.46 21.969H4.05c-.93 0-1.4-1.13-.74-1.79l5.73-5.73a4.189 4.189 0 0 1 5.93 0l5.73 5.73c.66.66.19 1.79-.74 1.79h-1.41c-.83 0-1.63-.33-2.22-.92l-3.58-3.58c-.41-.41-1.07-.41-1.48 0l-3.58 3.58c-.6.59-1.4.92-2.23.92Z" stroke="#ffffff" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                     </a>
-                    <a href="#" className="hover:text-gray-400">
+                    <a href="https://www.instagram.com/artformemories_rw?igsh=MXVxeTl0OHFndm8zdg==" className="hover:text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"><path d="M9 22h6c5 0 7-2 7-7V9c0-5-2-7-7-7H9C4 2 2 4 2 9v6c0 5 2 7 7 7Z" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path opacity=".34" d="M12 15.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Z" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path opacity=".34" d="M17.636 7h.012" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                     </a>
-                    <a href="#" className="hover:text-gray-400">
+                    <a href="https://youtube.com/@artformemories?si=--2YjUjM3TRFRino" className="hover:text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"><path d="M17 20H7c-3 0-5-2-5-5V9c0-3 2-5 5-5h10c3 0 5 2 5 5v6c0 3-2 5-5 5Z" stroke="#ffffff" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path><path opacity=".4" d="m11.42 9.49 2.47 1.48c.94.57.94 1.49 0 2.06l-2.47 1.48c-1 .6-1.82.14-1.82-1.03v-2.97c0-1.16.82-1.62 1.82-1.02Z" stroke="#ffffff" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                     </a>
                 </div>
