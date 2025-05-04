9a1d69b0d1b7378ce827dc4871eb3297a5738bf6
patiencemanzen <hseal419@gmail.com>
2025-05-04 20:13:02 +0200

    Refactor StoriesTable component: update story type display and remove unused file and status columns

--- src/components/dashboard/StoriesTable.tsx
+++ src/components/dashboard/StoriesTable.tsx
@@ -69,32 +69,7 @@ const StoriesTable: React.FC<TableProps> = ({ headers, data, onDelete, onPreview
                                 {Stories.author}
                             </td>
                             <td className="p-4">{new Date(Stories.date).toLocaleDateString()}</td>
-                            
-                            <td className="p-4">
-                                {Stories.StoryFile ? (
-                                    <a
-                                        href={Stories.StoryFile[0].url}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="text-blue-500 hover:underline cursor-pointer"
-                                    >
-                                        Open File
-                                    </a>
-                                ) : (
-                                    'No File'
-                                )}
-                            </td>
-                            
-                            <td className="p-4 text-sm">
-                                {Stories.status === 'approved' ? (
-                                    <span className="text-white bg-green-500 rounded-lg px-4 py-2 text-xsm">Approved</span>
-                                ) : Stories.status === 'rejected' ? (
-                                    <span className="bg-red-700  rounded-lg px-4 py-2 text-xsm text-white">Rejected</span>
-                                ) : (
-                                    <span className="bg-yellow-700  rounded-lg px-4 py-2 text-xsm text-white">Pending</span>
-                                )}
-                            </td>
-                            <td className="p-4">{Stories.storyType}</td>
+                            <td className="p-4">{Stories.type}</td>
                             <td className="p-4">
                                 <button onClick={() => onApproved && onApproved(Stories.id)} className="text-gray-500 hover:text-green-500 bg-green-200 rounded-xl p-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"><path opacity=".34" d="m8.38 11.998 2.41 2.42 4.83-4.84" stroke="#555555" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M10.75 2.45c.69-.59 1.82-.59 2.52 0l1.58 1.36c.3.26.86.47 1.26.47h1.7c1.06 0 1.93.87 1.93 1.93v1.7c0 .39.21.96.47 1.26l1.36 1.58c.59.69.59 1.82 0 2.52l-1.36 1.58c-.26.3-.47.86-.47 1.26v1.7c0 1.06-.87 1.93-1.93 1.93h-1.7c-.39 0-.96.21-1.26.47l-1.58 1.36c-.69.59-1.82.59-2.52 0l-1.58-1.36c-.3-.26-.86-.47-1.26-.47H6.18c-1.06 0-1.93-.87-1.93-1.93V16.1c0-.39-.21-.95-.46-1.25l-1.35-1.59c-.58-.69-.58-1.81 0-2.5l1.35-1.59c.25-.3.46-.86.46-1.25V6.2c0-1.06.87-1.93 1.93-1.93h1.73c.39 0 .96-.21 1.26-.47l1.58-1.35Z" stroke="#555555" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
